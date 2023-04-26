72004fbd80fe0b47643bd1a32597b0276b7c2272
Diego Mauricio Pardo Calderón <128651145+DiegoPardoCalderon@users.noreply.github.com>
2023-04-25 21:33:25 -0400

    Version final ejercicio vuela ya!

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ReboundM3S4
 Ejercicio Rebound Modulo 3 Sesión 4 Vuela Ya!
+
+Una aerolínea internacional te ha contratado para generar una vista nueva en su sitio de Internet. Varias encuestas han demostrado que el factor más tomado en cuenta por sus pasajeros antes de comprar un vuelo, es si éste tiene escalas, o no. Por esta razón, se requiere desarrollar una vista sencilla, que permita ingresar los datos de origen y destino, para desplegar en un itinerario si el vuelo tiene o no escala.
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,53 @@
-var origen = document.querySelector('#origen');
-    origen.addEventListener('change, function ({
-        var resultadoorigen = document.querySelector('#resultadoorigen');
+$(document).ready(function() {
+
+    $(document).on("change","#origen", function(){
+        mostrarEscalas()
+        var origen = $('option:selected', "#origen").text()
+        $("#resultado-origen").html(origen)
         
-     }),
+    })
+
+    $(document).on("change","#destino", function(){
+        mostrarEscalas()
+        var destino = $('option:selected',"#destino").text()
+        $("#resultado-destino").html(destino)
+    })
+
+    $(document).on("input", "#fecha-origen", function(){
+        mostrarEscalas()
+        var fechaOrigen = $("#fecha-origen").val()
+        $("#resultado-fecha-origen").html(fechaOrigen)
+    })
+
+    $(document).on("input", "#fecha-destino", function(){
+        mostrarEscalas()
+        var fechaDestino = $("#fecha-destino").val()
+        $("#resultado-fecha-destino").html(fechaDestino)
+    })
+
+
+
+    function mostrarEscalas(){
+        var origen = $("#origen").val()
+        var destino = $("#destino").val()
+        var fechaOrigen = $("#fecha-origen").val()
+        var fechaDestino = $("#fecha-destino").val()
+
+        var mostrarItinerario = origen !== "" && destino !== "" && fechaOrigen !== "" && fechaDestino !== ""
+
+        
+        if(mostrarItinerario){
+            $("#contenedor-itinerario").removeClass("d-none")
+        }else {
+            $("#contenedor-itinerario").addClass("d-none")
+        }
+
+        var siTieneEscala = (origen === "chicago" && destino === "venecia") ||
+                            (origen === "boston" && destino === "paris")
+
+        var mensajeEscala = siTieneEscala ? "¡Ojo! Tu vuelo tiene una escala" : "Tu vuelo no tiene escala"
+        $("#resultado-escala").html(mensajeEscala)
+
+    }
+
+})
